a942c10a481a117cd240126048dd73c34c726800
Jarosław Wołoszyn <tumrys@gline.webd.pl>
2024-01-28 02:16:16 +0100

    Version 19 (1.13)

--- toplistadiscopolo/src/main/java/com/grandline/toplistadiscopolo/FileCache.java
+++ toplistadiscopolo/src/main/java/com/grandline/toplistadiscopolo/FileCache.java
@@ -3,6 +3,7 @@ package com.grandline.toplistadiscopolo;
 import android.content.Context;
 
 import java.io.File;
+import java.nio.file.SecureDirectoryStream;
 
 public class FileCache {
     
@@ -10,30 +11,29 @@ public class FileCache {
     
     public FileCache(Context context){
         //Find the dir to save cached images
-        if (android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED))
-            cacheDir=new File(android.os.Environment.getExternalStorageDirectory(),"LazyList");
-        else
+     ///   if (android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED))
+     ///       cacheDir=new File(android.os.Environment.getExternalStorageDirectory(),"LazyList");
+     ///   else
             cacheDir=context.getCacheDir();
-        if(!cacheDir.exists())
-            cacheDir.mkdirs();
+      //  if(!cacheDir.exists())
+      //      cacheDir.mkdirs();
     }
     
     public File getFile(String url){
         //I identify images by hashcode. Not a perfect solution, good for the demo.
         String filename=String.valueOf(url.hashCode());
         //Another possible solution (thanks to grantland)
-        //String filename = URLEncoder.encode(url);
-        File f = new File(cacheDir, filename);
-        return f;
-        
+       // String filename = URLEncoder.encode(url);
+        return new File(cacheDir, filename);
     }
     
     public void clear(){
         File[] files=cacheDir.listFiles();
         if(files==null)
             return;
+        SecureDirectoryStream<Object> context;
         for(File f:files)
-            f.delete();
+        f.delete();
     }
 
 }
